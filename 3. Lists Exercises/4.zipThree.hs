8111c658359479cfee5eff9e3ca99e99c697539a
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2019-01-07 22:34:02 +0200

    Update 4.zipThree.hs

--- 3. Lists Exercises/4.zipThree.hs
+++ 3. Lists Exercises/4.zipThree.hs
@@ -1,2 +1,5 @@
+--zipThree like zip but with 3 lists
 zipThree :: [Int] -> [Int] -> [Int] -> [(Int, Int, Int)]
-zipThree aList bList cList = [(a,b,c) | a <- aList, b <- bList, c <- cList, a == b && b == c]
+zipThree aList bList cList = [(a,b,c) | a <- aList, b <- bList, c <- cList]
+
+--zipThree using only map and zip
